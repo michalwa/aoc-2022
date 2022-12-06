fe95a62a2368b96fb21d4f8806cbcc7316c98139
michalwa <michalwa2003@gmail.com>
2022-12-06 15:52:26 +0100

    Day 4 Prolog tweaks and part 1

--- src/day_4.pl
+++ src/day_4.pl
@@ -5,13 +5,14 @@
 :- use_module(library(pio)).
 :- use_module(library(lists)).
 :- use_module(library(clpz)).
+:- use_module(library(dif)).
 
 range_pairs([])     --> [].
 range_pairs([P|Ps]) --> range_pair(P), "\n", range_pairs(Ps).
 
-range_pair((As-Ae)-(Bs-Be)) --> range(As, Ae), ",", range(Bs, Be).
+range_pair(A-B) --> range(A), ",", range(B).
 
-range(S, E) --> integer(S), "-", integer(E).
+range(S-E) --> integer(S), "-", integer(E).
 
 integer(I) --> digits(Ds), { number_chars(I, Ds) }.
 
@@ -20,12 +21,24 @@ digits_r([D|Ds]) --> digit(D), digits_r(Ds).
 digits_r([])     --> [].
 digit(D)         --> [D], { char_type(D, decimal_digit) }.
 
-% Part 2
+range_subrange(As-Ae, Bs-Be) :- As #=< Bs, Be #=< Ae.
+
 ranges_overlap(As-Ae, Bs-Be) :- As #=< Be, Ae #>= Bs.
 
 run :-
     phrase_from_file(range_pairs(Ps), 'input/day_4/input.txt'),
-    findall((A-B), (member(A-B, Ps), ranges_overlap(A, B)), Rs),
+    findall(A-B, (
+        member(A-B, Ps),
+
+        % Part 1
+        ( range_subrange(A, B)
+        % We have to assert dif(A, B) here, because otherwise the second goal
+        % of the disjuction counts as a duplicate solution if `A = B`
+        ; dif(A, B), range_subrange(B, A) )
+
+        % Part 2
+        % ranges_overlap(A, B)
+    ), Rs),
     length(Rs, Count),
     write(Count), nl,
     halt.
