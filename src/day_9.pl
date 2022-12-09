e35a6f2d6eb96d783555126fd7cd4136c8c4a83f
michalwa <michalwa2003@gmail.com>
2022-12-09 12:35:49 +0100

    Day 9 part 2

--- src/day_9.pl
+++ src/day_9.pl
@@ -21,52 +21,67 @@ digit(D)             --> [D], { char_type(D, decimal_digit) }.
 % Logic
 step(_-0, R, R, []). % for completeness
 
-step('L'-1, (Hx0,Hy)-T0, P, [P]) :-
-    P = (Hx, Hy)-T,
+step('L'-1, [(Hx0,Hy)|Ts0], R, [R]) :-
+    R = [(Hx,Hy)|Ts],
     Hx #= Hx0 - 1,
-    follow((Hx,Hy), T0, T).
+    iterate_follow((Hx,Hy), Ts0, Ts).
 
-step('U'-1, (Hx,Hy0)-T0, P, [P]) :-
-    P = (Hx,Hy)-T,
+step('U'-1, [(Hx,Hy0)|Ts0], R, [R]) :-
+    R = [(Hx,Hy)|Ts],
     Hy #= Hy0 + 1,
-    follow((Hx,Hy), T0, T).
+    iterate_follow((Hx,Hy), Ts0, Ts).
 
-step('R'-1, (Hx0,Hy)-T0, P, [P]) :-
-    P = (Hx,Hy)-T,
+step('R'-1, [(Hx0,Hy)|Ts0], R, [R]) :-
+    R = [(Hx,Hy)|Ts],
     Hx #= Hx0 + 1,
-    follow((Hx,Hy), T0, T).
+    iterate_follow((Hx,Hy), Ts0, Ts).
 
-step('D'-1, (Hx,Hy0)-T0, P, [P]) :-
-    P = (Hx,Hy)-T,
+step('D'-1, [(Hx,Hy0)|Ts0], R, [R]) :-
+    R = [(Hx,Hy)|Ts],
     Hy #= Hy0 - 1,
-    follow((Hx,Hy), T0, T).
+    iterate_follow((Hx,Hy), Ts0, Ts).
 
-step(D-S, R0, R, [P|Ps]) :-
+step(D-S, R0, R, [P|Rs]) :-
     S #> 1, S1 #= S - 1,
     step(D-1, R0, R1, [P]),
-    step(D-S1, R1, R, Ps).
+    step(D-S1, R1, R, Rs).
 
+% FIXME: Incredibly slow
 follow((Hx, Hy), (Tx0, Ty0), (Tx, Ty)) :-
-    ( Hx - Tx0 #=  2, Tx #= Hx - 1, Ty #= Hy
-    ; Hx - Tx0 #= -2, Tx #= Hx + 1, Ty #= Hy
-    ; Hy - Ty0 #=  2, Tx #= Hx,     Ty #= Hy - 1
-    ; Hy - Ty0 #= -2, Tx #= Hx,     Ty #= Hy + 1
-    ), !.
-follow(_, T, T).
+    Dx #= Hx - Tx0,
+    Dy #= Hy - Ty0,
+    ( [Dx, Dy] ins -1'..'1,            Tx #= Tx0, Ty #= Ty0
+    ; ( abs(Dx) #= 2 ; abs(Dy) #= 2 ), Tx #= Tx0 + sign(Dx), Ty #= Ty0 + sign(Dy) ).
 
-iterate_steps(Is, Ps) :- iterate_steps_(Is, (0,0)-(0,0), Ps).
-iterate_steps_([], _, []).
-iterate_steps_([I|Is], P0, Ps) :-
-    step(I, P0, P, Ps1),
-    iterate_steps_(Is, P, Ps2),
-    append(Ps1, Ps2, Ps).
+iterate_follow(_, [], []).
+iterate_follow(H, [T0|Ts0], [T|Ts]) :-
+    follow(H, T0, T),
+    iterate_follow(T, Ts0, Ts).
 
-pair_second(_-X, X).
+simulation_steps(RopeLength, Is, Rs) :-
+    R0 = [(0,0)|_],
+    same(R0),
+    length(R0, RopeLength),
+    simulation_steps_(Is, R0, Rs).
+
+simulation_steps_([], _, []).
+simulation_steps_([I|Is], R0, Rs) :-
+    step(I, R0, R, Rs1),
+    simulation_steps_(Is, R, Rs2),
+    append(Rs1, Rs2, Rs).
+
+same([]).
+same([_]).
+same([X,X|Xs]) :- same([X|Xs]).
+
+last([X], X).
+last([_|Xs], X) :- last(Xs, X).
 
 run :-
     phrase_from_file(instructions(Is), 'input/day_9/input.txt'),
-    iterate_steps(Is, Ls),
-    maplist(pair_second, Ls, Ts),
+    % Part 1: simulation_steps(2, Is, Rs),
+    simulation_steps(10, Is, Rs),
+    maplist(last, Rs, Ts),
     list_to_set(Ts, UTs),
     length(UTs, N),
     portray_clause(N),
