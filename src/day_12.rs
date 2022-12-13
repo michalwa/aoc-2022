eddcdc3c0befac4ffc089d33e21fdbac4883b0fb
michalwa <michalwa2003@gmail.com>
2022-12-13 13:19:21 +0100

    Tidy up day 12

--- src/day_12.rs
+++ src/day_12.rs
@@ -30,12 +30,15 @@ pub fn solve(input: BufReader<File>) {
     // let path = astar(&rows, start.unwrap(), end);
     // println!("{}", path.len() - 1);
 
+    // Part 2
     let mut min_len = usize::MAX;
 
     for (i, row) in rows.iter().enumerate() {
         for (j, c) in row.iter().enumerate() {
             if elevation(*c) == b'a' {
-                if let Some(path) = astar(&rows, [i, j], end) {
+                let mut astar = Astar::new(&rows, [i, j], end);
+
+                if let Some(path) = astar.find_path() {
                     min_len = min_len.min(path.len());
                 }
             }
@@ -45,90 +48,112 @@ pub fn solve(input: BufReader<File>) {
     println!("{}", min_len - 1);
 }
 
-// bardzo przepraszam za ten kod
-
-fn astar(
-    rows: &[Vec<u8>],
+struct Astar<'a> {
+    rows: &'a [Vec<u8>],
     start: [usize; 2],
     end: [usize; 2],
-) -> Option<Vec<[usize; 2]>> {
-    let h =
-        |n: [usize; 2]| (end[0].abs_diff(n[0]) + end[1].abs_diff(n[1])) as u32;
+    open_set: HashSet<[usize; 2]>,
+    came_from: HashMap<[usize; 2], [usize; 2]>,
+    g_score: HashMap<[usize; 2], u32>,
+    f_score: HashMap<[usize; 2], u32>,
+}
 
-    let neighbors = |[i, j]: [usize; 2]| {
-        let mut ns = Vec::new();
+impl<'a> Astar<'a> {
+    fn new(rows: &'a [Vec<u8>], start: [usize; 2], end: [usize; 2]) -> Self {
+        Self {
+            rows,
+            start,
+            end,
+            open_set: HashSet::from([start]),
+            came_from: HashMap::new(),
+            g_score: HashMap::new(),
+            f_score: HashMap::new(),
+        }
+    }
 
-        for [di, dj] in [[-1, 0], [1, 0], [0, -1], [0, 1]] {
-            let ni = i as isize + di;
-            let nj = j as isize + dj;
+    fn find_path(&mut self) -> Option<Vec<[usize; 2]>> {
+        self.g_score.clear();
+        self.g_score.insert(self.start, 0);
 
-            if !(0..rows.len() as isize).contains(&ni)
-                || !(0..rows[0].len() as isize).contains(&nj)
-            {
-                continue;
+        self.f_score.clear();
+        self.f_score.insert(self.start, self.heuristic(self.start));
+
+        while !self.open_set.is_empty() {
+            let current = self
+                .open_set
+                .iter()
+                .copied()
+                .min_by_key(|n| self.f_score.get(n).unwrap_or(&u32::MAX))
+                .unwrap();
+
+            if current == self.end {
+                return Some(self.reconstruct_path(current));
             }
 
-            let e = elevation(rows[i][j]);
-            let ne = elevation(rows[ni as usize][nj as usize]);
+            self.open_set.remove(&current);
 
-            if ne <= e + 1 {
-                ns.push([ni as usize, nj as usize]);
+            for neighbor in self.neighbors(current).collect::<Vec<_>>() {
+                let new_g_score = self.g_score[&current] + 1;
+
+                if !self.g_score.contains_key(&neighbor)
+                    || new_g_score < self.g_score[&neighbor]
+                {
+                    self.came_from.insert(neighbor, current);
+                    self.g_score.insert(neighbor, new_g_score);
+                    self.f_score.insert(neighbor, new_g_score + self.heuristic(neighbor));
+                    self.open_set.insert(neighbor);
+                }
             }
         }
 
-        ns
-    };
+        None
+    }
 
-    fn reconstruct_path(
-        came_from: HashMap<[usize; 2], [usize; 2]>,
-        mut current: [usize; 2],
-    ) -> Vec<[usize; 2]> {
+    fn heuristic(&self, cell: [usize; 2]) -> u32 {
+        (cell[0].abs_diff(self.end[0]) + cell[1].abs_diff(self.end[1])) as u32
+    }
+
+    fn neighbors(
+        &self,
+        cell: [usize; 2],
+    ) -> impl Iterator<Item = [usize; 2]> + '_ {
+        let cell_elevation = elevation(self.rows[cell[0]][cell[1]]);
+
+        [[-1, 0], [1, 0], [0, -1], [0, 1]]
+            .into_iter()
+            .filter_map(move |offset| self.offset(cell, offset))
+            .filter(move |&[i, j]| {
+                elevation(self.rows[i][j]) <= cell_elevation + 1
+            })
+    }
+
+    fn offset(
+        &self,
+        cell: [usize; 2],
+        offset: [isize; 2],
+    ) -> Option<[usize; 2]> {
+        let new_cell =
+            [cell[0] as isize + offset[0], cell[1] as isize + offset[1]];
+
+        if (0..self.rows.len() as isize).contains(&new_cell[0])
+            && (0..self.rows[0].len() as isize).contains(&new_cell[1])
+        {
+            Some([new_cell[0] as usize, new_cell[1] as usize])
+        } else {
+            None
+        }
+    }
+
+    fn reconstruct_path(&mut self, mut current: [usize; 2]) -> Vec<[usize; 2]> {
         let mut path = vec![current];
 
-        while came_from.contains_key(&current) {
-            current = came_from[&current];
+        while self.came_from.contains_key(&current) {
+            current = self.came_from[&current];
             path.insert(0, current);
         }
 
         path
     }
-
-    let mut open_set = HashSet::from([start]);
-    let mut came_from: HashMap<[usize; 2], [usize; 2]> = HashMap::new();
-
-    let mut g_score: HashMap<[usize; 2], u32> = HashMap::new();
-    g_score.insert(start, 0);
-
-    let mut f_score: HashMap<[usize; 2], u32> = HashMap::new();
-    f_score.insert(start, h(start));
-
-    while !open_set.is_empty() {
-        let current = open_set
-            .iter()
-            .copied()
-            .min_by_key(|n| f_score.get(n).unwrap_or(&u32::MAX))
-            .unwrap();
-
-        if current == end {
-            return Some(reconstruct_path(came_from, current));
-        }
-
-        open_set.remove(&current);
-
-        for n in neighbors(current) {
-            let tent_g_score = g_score[&current] + 1;
-
-            if !g_score.contains_key(&n) || tent_g_score < g_score[&n] {
-                came_from.insert(n, current);
-                g_score.insert(n, tent_g_score);
-                f_score.insert(n, tent_g_score + h(n));
-
-                open_set.insert(n);
-            }
-        }
-    }
-
-    None
 }
 
 fn elevation(byte: u8) -> u8 {
